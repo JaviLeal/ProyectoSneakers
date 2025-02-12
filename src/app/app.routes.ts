4c5eaf1e12b892a3720e99c96380f743cbb07ffe
JaviLeal <javierleal2005@gmail.com>
2025-02-12 14:09:33 +0100

    Cuarto Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SneakersListComponent } from './modules/sneakers/pages/sneakers-list/sneakers-list.component';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { RegisterComponent } from './modules/auth/pages/register/register.component';
+import { CartViewComponent } from './modules/cart/pages/cart-view/cart-view.component';
+import { AboutComponent } from './modules/users/pages/about/about.component';
+import { ContactComponent } from './modules/users/pages/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirige a sneakers por defecto
   { path: 'home', component: HomeComponent },
   { path: 'productos', component: SneakersListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'cart', component: CartViewComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' } // Manejo de rutas no encontradas
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from '../modules/auth/pages/login/login.component';
+import { RegisterComponent } from '../modules/auth/pages/register/register.component';
+import { CartViewComponent } from '../modules/cart/pages/cart-view/cart-view.component';
 import { RouterModule } from '@angular/router';
+import { AboutComponent } from '../modules/users/pages/about/about.component';
+import { ContactComponent } from '../modules/users/pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartViewComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
@@ -18,7 +28,12 @@ import { RouterModule } from '@angular/router';
   exports: [
     HeaderComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartViewComponent,
+    AboutComponent,
+    ContactComponent
   ]
 })
 export class SharedModule { }
